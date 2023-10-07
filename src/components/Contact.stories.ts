6aef1beb23f07d90e04390bea2c43f6d3fa9890f
McBay Balatbat <mcbaybalatbat5@gmail.com>
2023-10-07 08:43:36 -0300

    update storybook

--- src/components/Contact.stories.ts
+++ src/components/Contact.stories.ts
@@ -2,13 +2,14 @@ import type { Meta, StoryObj } from "@storybook/vue3";
 import Contact from "./Contact.vue";
 
 const meta = {
-  title: "Example/Contact",
+  title: "Components/Contact",
   component: Contact,
-  tags: ["autodocs"],
-  argTypes: {
-    close: { action: "closed" },
-  },
+  argTypes: {},
 } satisfies Meta<typeof Contact>;
 
 export default meta;
 type Story = StoryObj<typeof meta>;
+
+export const Primary: Story = {
+  args: {},
+};
--- src/components/MainConversion.stories.ts
+++ src/components/MainConversion.stories.ts
@@ -2,10 +2,14 @@ import type { Meta, StoryObj } from "@storybook/vue3";
 import MainConversion from "./MainConversion.vue";
 
 const meta = {
-  title: "Example/MainConversion",
+  title: "Components/MainConversion",
   component: MainConversion,
   argTypes: {},
 } satisfies Meta<typeof MainConversion>;
 
 export default meta;
 type Story = StoryObj<typeof meta>;
+
+export const Primary: Story = {
+  args: {},
+};
