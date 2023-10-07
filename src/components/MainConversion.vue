452871b1a333288e4c15834e3f17a587f7e05fea
Yuriy Leve <yuriyleve@gmail.com>
2023-10-07 01:53:18 -0300

    make cta component

---
+++ src/components/MainConversion.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+import Contact from "./Contact.vue";
+</script>
+
+<template>
+    <div class="cta">
+        <div class="title">
+            <div class="header">
+                <p>CtA - Main conversion</p>
+            </div>
+        </div>
+        <div class="content">
+            <div class=" left">
+                <div class="actionTitle">
+                    A wonderful serenity has taken possessio
+                </div>
+                <div class="actionDescription">
+                    A wonderful serenity has taken possessio of my entire soul, like these sweet mo
+                </div>
+                <button class="button">Link button</button>
+            </div>
+            <div class=" right">
+                <img src="../assets/aaa.jpg" class="" alt="" />
+            </div>
+        </div>
+    </div>
+    <Contact></Contact>
+</template>
